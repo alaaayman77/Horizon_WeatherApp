36cb91150b28ac8eb94af41f4e46ecd69c1b304f
alaaayman77 <alaaaymanfouad@gmail.com>
2026-06-21 08:49:39 -0700

    added weather service

---
+++ Horizon/data/network/service/WeatherService.swift
@@ -0,0 +1,26 @@
+//
+//  ForecastService.swift
+//  Horizon
+//
+//  Created by Alaa Ayman on 21/06/2026.
+//
+
+import Foundation
+
+class WeatherService {
+    private let network: NetworkService
+    
+    init(network: NetworkService = NetworkManager()) {
+        self.network = network
+    }
+    
+    func fetchForecast(query: String, days: Int) async throws -> WeatherResponseDTO {
+        let endpoint = WeatherEndpoint.forecast(query: query, days: days)
+        return try await network.fetchData(url: endpoint.fullURL, parameters: endpoint.parameters)
+    }
+    
+    func fetchCurrentWeather(query: String) async throws -> WeatherResponseDTO {
+        let endpoint = WeatherEndpoint.current(query: query)
+        return try await network.fetchData(url: endpoint.fullURL, parameters: endpoint.parameters)
+    }
+}
